3493bb537a7b92b156fdc1804add8eba69a0fd89
Alexandru Bianca <biancaalexandru19@gmail.com>
2023-07-19 22:47:10 +0300

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+
+
+    if(number>0){
+        aux=string; 
+        for (let x=1; x<number; x++){
+            string=string.concat(aux); 
+        }
+        return string; 
+    }
+    else if(number==0)
+    {
+        return ""; 
+    }
+    else
+    {
+        return "ERROR"; 
+    }
 
 };
 
